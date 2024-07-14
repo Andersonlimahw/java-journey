a2be9d31bb70c51b35b8d10531be0374b399a790
andersonlimadev <andersonlimahw@gmail.com>
2024-07-14 05:49:33 -0300

    🆕 feature: trips: integrando com banco de dados

--- src/main/java/com/lemon/planner/trip/TripController.java
+++ src/main/java/com/lemon/planner/trip/TripController.java
@@ -1,5 +1,7 @@
 package com.lemon.planner.trip;
 
+import com.lemon.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/trips")
 
 public class TripController {
+    @Autowired
+    private TripRepository repository;
+
+    @Autowired
+    private ParticipantService participantService;
+
     @PostMapping
-    public ResponseEntity<String> createTrip(@RequestBody TripRequestPayload payload) {
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
 
         Trip trip = new Trip(payload);
-
-        return ResponseEntity.ok("Trip created");
+        this.repository.save(trip);
+        this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), trip);
+        TripCreateResponse response = new TripCreateResponse(trip.getId());
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/lemon/planner/trip/TripCreateResponse.java
@@ -0,0 +1,6 @@
+package com.lemon.planner.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(UUID uuid) {
+}
